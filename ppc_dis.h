2f4e559ae9bb77b13b765c75362fb8eaf46d6327
vikas <vi@vi.vi>
2013-11-11 00:18:22 +0545

    Revert "Changing License to LGPLv2.1"
    This reverts commit 35957eb62800355a2251eab9870b50d6f66c8412.

--- ppc_dis.h
+++ ppc_dis.h
@@ -10,7 +10,7 @@
 //
 //
 //  This library is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License Version 2.1 as
+//  it under the terms of the GNU General Public License Version 2 as
 //  published by the Free Software Foundation.
 //
 //  It is distributed in the hope that it will be useful, but WITHOUT
@@ -18,7 +18,7 @@
 //  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
 //  License for more details.
 //
-//  You should have received a copy of the GNU Lesser General Public License
+//  You should have received a copy of the GNU General Public License
 //  along with this file; see the file COPYING.  If not, write to the
 //  Free Software Foundation, 51 Franklin Street - Fifth Floor, Boston,
 //  MA 02110-1301, USA.
